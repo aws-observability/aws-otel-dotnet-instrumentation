406cf3707315815cbbf6a2b01196a495da7fd8c0
Mohamed Asaker <asakem@amazon.com>
2024-05-21 14:29:56 -0700

    added init

--- src/AWS.OpenTelemetry.AutoInstrumentation/Plugin.cs
+++ src/AWS.OpenTelemetry.AutoInstrumentation/Plugin.cs
@@ -1,6 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Contrib.Extensions.AWSXRay.Resources;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -13,6 +17,17 @@ namespace AWS.OpenTelemetry.AutoInstrumentation;
 /// </summary>
 public class Plugin
 {
+    private static readonly ILoggerFactory Factory = LoggerFactory.Create(builder => builder.AddConsole());
+    private static readonly ILogger Logger = Factory.CreateLogger<AwsMetricAttributeGenerator>();
+    private static readonly string APP_SIGNALS_ENABLED_CONFIG = "OTEL_AWS_APP_SIGNALS_ENABLED";
+    private static readonly string ApplicationSignalsEnabledConfig = "OTEL_AWS_APPLICATION_SIGNALS_ENABLED";
+    private static readonly string APP_SIGNALS_EXPORTER_ENDPOINT_CONFIG = "OTEL_AWS_APP_SIGNALS_EXPORTER_ENDPOINT";
+    private static readonly string ApplicationSignalsExporterEndpointConfig = "OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT";
+    private static readonly string MetricExportIntervalConfig = "OTEL_METRIC_EXPORT_INTERVAL";
+    private static readonly int DefaultMetricExportInternal = 60000;
+    private static readonly string OtelTracesSampler = "OTEL_TRACES_SAMPLER";
+    private static readonly string OtelTracesSamplerArg = "OTEL_TRACES_SAMPLER_ARG";
+
     /// <summary>
     /// To configure plugin, before OTel SDK configuration is called.
     /// </summary>public void Initializing()
@@ -27,7 +42,51 @@ public class Plugin
     /// <param name="tracerProvider"><see cref="TracerProvider"/> Provider to configure</param>
     public void TracerProviderInitialized(TracerProvider tracerProvider)
     {
-        // My custom logic here
+        if (this.IsApplicationSignalsEnabled())
+        {
+            tracerProvider.AddProcessor(AttributePropagatingSpanProcessorBuilder.Create().Build());
+
+            string? intervalConfigString = System.Environment.GetEnvironmentVariable(MetricExportIntervalConfig);
+            int exportInterval = DefaultMetricExportInternal;
+            try
+            {
+                exportInterval = Convert.ToInt32(intervalConfigString);
+            }
+            catch (Exception)
+            {
+                Logger.Log(LogLevel.Trace, "Could not convert OTEL_METRIC_EXPORT_INTERVAL to integer. Using default value 60000.");
+            }
+
+            if (exportInterval.CompareTo(DefaultMetricExportInternal) > 0)
+            {
+                exportInterval = DefaultMetricExportInternal;
+                Logger.Log(LogLevel.Information, "AWS Application Signals metrics export interval capped to {0}", exportInterval);
+            }
+
+            // https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#enable-metric-exporter
+            // for setting the temporatityPref.
+            var metricReader = new PeriodicExportingMetricReader(this.ApplicationSignalsExporterProvider(), exportInterval);
+            MeterProvider provider = Sdk.CreateMeterProviderBuilder()
+            .AddReader(metricReader)
+            .ConfigureResource(builder => builder
+                .AddDetector(new AWSEC2ResourceDetector())
+                .AddDetector(new AWSECSResourceDetector())
+                .AddDetector(new AWSEKSResourceDetector()))
+            .AddMeter("AwsSpanMetricsProcessor")
+            .Build();
+
+            Resource resource = provider.GetResource();
+            BaseProcessor<Activity> spanMetricsProcessor = AwsSpanMetricsProcessorBuilder.Create(resource).Build();
+            tracerProvider.AddProcessor(spanMetricsProcessor);
+        }
+    }
+
+    private OtlpMetricExporter ApplicationSignalsExporterProvider()
+    {
+        // https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/metrics/customizing-the-sdk#configuring-the-aggregation-of-a-histogram
+        // use the above to check about adding aggregation type.
+        var options = new OtlpExporterOptions();
+        return new OtlpMetricExporter(options);
     }
 
     /// <summary>
@@ -57,8 +116,61 @@ public class Plugin
     /// <returns>Returns configured builder</returns>
     public TracerProviderBuilder AfterConfigureTracerProvider(TracerProviderBuilder builder)
     {
-        // My custom logic here
+        if (this.IsApplicationSignalsEnabled())
+        {
+            Logger.Log(LogLevel.Information, "AWS Application Signals enabled");
+            Sampler alwaysRecordSampler = AlwaysRecordSampler.Create(this.GetSampler());
+            builder.SetSampler(alwaysRecordSampler);
+        }
+
+        builder.AddXRayTraceId();
         return builder;
+    }
+
+    // This function is based on an internal function in Otel:
+    // https://github.com/open-telemetry/opentelemetry-dotnet/blob/1bbafaa7b7bed6470ff52fc76b6e881cd19692a5/src/OpenTelemetry/Trace/TracerProviderSdk.cs#L408
+    // Unfortunately, that function is private.
+    private Sampler GetSampler()
+    {
+        string? tracesSampler = System.Environment.GetEnvironmentVariable(OtelTracesSampler);
+        string? tracesSamplerArg = System.Environment.GetEnvironmentVariable(OtelTracesSamplerArg);
+        double samplerProbability = 1.0;
+        if (tracesSampler != null)
+        {
+            try
+            {
+                samplerProbability = Convert.ToDouble(tracesSamplerArg);
+            }
+            catch (Exception)
+            {
+                Logger.Log(LogLevel.Trace, "Could not convert OTEL_TRACES_SAMPLER_ARG to double. Using default value 1.0.");
+            }
+        }
+
+        // based on the list of available samplers:
+        // https://github.com/open-telemetry/opentelemetry-dotnet-instrumentation/blob/77256e3a9666ee0f1f72fec5f4ca1a6d8500f229/docs/config.md#samplers
+        // Need to also add XRay Sampler here.
+        // Currently, this is the only way to get the sampler as there is no factory and we can't get the sampler
+        // that was already set in the TracerProviderBuilder
+        switch (tracesSampler)
+        {
+            case "always_on":
+                return new AlwaysOnSampler();
+            case "always_off":
+                return new AlwaysOffSampler();
+            case "traceidratio":
+                return new TraceIdRatioBasedSampler(samplerProbability);
+            case "parentbased_always_off":
+                Sampler alwaysOffSampler = new AlwaysOffSampler();
+                return new ParentBasedSampler(alwaysOffSampler);
+            case "parentbased_traceidratio":
+                Sampler traceIdRatioSampler = new TraceIdRatioBasedSampler(samplerProbability);
+                return new ParentBasedSampler(traceIdRatioSampler);
+            case "parentbased_always_on":
+            default:
+                Sampler alwaysOnSampler = new AlwaysOnSampler();
+                return new ParentBasedSampler(alwaysOnSampler);
+        }
     }
 
     /// <summary>
@@ -68,6 +180,32 @@ public class Plugin
     /// </summary>
     /// <param name="options"><see cref="OtlpExporterOptions"/> options to configure</param>
     public void ConfigureTracesOptions(OtlpExporterOptions options)
+    {
+        if (this.IsApplicationSignalsEnabled())
+        {
+            Logger.Log(
+          LogLevel.Debug, "AWS Application Signals export protocol: %{0}", options.Protocol);
+
+            string? applicationSignalsEndpoint = System.Environment.GetEnvironmentVariable(ApplicationSignalsExporterEndpointConfig);
+            if (options.Protocol == OtlpExportProtocol.HttpProtobuf)
+            {
+                applicationSignalsEndpoint = applicationSignalsEndpoint ?? "http://localhost:4316/v1/metrics";
+            }
+            else if (options.Protocol == OtlpExportProtocol.Grpc)
+            {
+                applicationSignalsEndpoint = applicationSignalsEndpoint ?? "http://localhost:4315";
+            }
+            else
+            {
+                throw new NotSupportedException("Unsupported AWS Application Signals export protocol: " + options.Protocol);
+            }
+
+            options.Endpoint = new Uri(applicationSignalsEndpoint);
+        }
+    }
+
+    // To configure any metrics options used by OpenTelemetry .NET Automatic Instrumentation
+    public void ConfigureMetricsOptions(MetricReaderOptions options)
     {
         // My custom logic here
         // Find supported options below
@@ -119,5 +257,10 @@ public class Plugin
         // This method could be overridden by ConfigureTracesOptions, ConfigureMeterProvider and ConfigureLogsOptions
         // by calling SetResourceBuilder with new object.
         return builder;
+    }
+
+    private bool IsApplicationSignalsEnabled()
+    {
+        return System.Environment.GetEnvironmentVariable(ApplicationSignalsEnabledConfig) == "true";
     }
 }
