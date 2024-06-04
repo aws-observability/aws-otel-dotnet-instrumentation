b148c635cfe8bed519d5d527b43c6247fed8e1d8
Xinran Zhang <xinranzh@amazon.com>
2024-06-04 09:57:21 -0700

    Another fail try

--- test/AWS.OpenTelemetry.AutoInstrumentation.Tests/AttributePropagatingSpanProcessorTest.cs
+++ test/AWS.OpenTelemetry.AutoInstrumentation.Tests/AttributePropagatingSpanProcessorTest.cs
@@ -3,23 +3,27 @@ using System.Diagnostics;
 using System.Reflection;
 using OpenTelemetry;
 using OpenTelemetry.Trace;
-
+using Moq;
+using OpenTelemetry.Resources;
 
 namespace AWS.OpenTelemetry.AutoInstrumentation.Tests;
 
 public class AttributePropagatingSpanProcessorTest
 {
     private Func<Activity, string> spanNameExtractor = AwsSpanProcessingUtil.GetIngressOperation;
+    private AwsSpanMetricsProcessor awsSpanMetricsProcessor;
+    private Mock<AwsMetricAttributeGenerator> Generator = new Mock<AwsMetricAttributeGenerator>();
+    private Resource resource = Resource.Empty;
     private string spanNameKey = "spanName";
     private string testKey1 = "key1";
     private string testKey2 = "key2";
     private TracerProviderBuilder tracerProviderBuilder = Sdk.CreateTracerProviderBuilder();
     private Tracer tracer;
     private ActivitySource activitySource = new ActivitySource("test");
+    private AttributePropagatingSpanProcessor attributePropagatingSpanProcessor;
 
     public AttributePropagatingSpanProcessorTest()
     {
-        
         ReadOnlyCollection<string> attributesKeysToPropagate = new ReadOnlyCollection<string>([testKey1, testKey2]);
         var listener = new ActivityListener
         {
@@ -27,9 +31,11 @@ public class AttributePropagatingSpanProcessorTest
             Sample = ((ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllData)
         };
         ActivitySource.AddActivityListener(listener);
+        attributePropagatingSpanProcessor=
+            AttributePropagatingSpanProcessor.Create(spanNameExtractor, spanNameKey, attributesKeysToPropagate);
+        tracerProviderBuilder.AddProcessor(attributePropagatingSpanProcessor);
         tracerProviderBuilder.AddSource(["test"]);
-        var sdkTracerProvider = tracerProviderBuilder.AddProcessor(AttributePropagatingSpanProcessor.Create(spanNameExtractor, spanNameKey,
-            attributesKeysToPropagate)).Build();
+        var sdkTracerProvider = tracerProviderBuilder.Build();
         tracer = sdkTracerProvider.GetTracer("awsxray");
     }
 
@@ -52,7 +58,14 @@ public class AttributePropagatingSpanProcessorTest
             return parentSpan;
         }
 
-        TelemetrySpan childSpan = tracer.StartSpan("child:" + depth, parentContext: parentSpan.Context);
+        TelemetrySpan childSpan = tracer.StartSpan("child:" + depth, SpanKind.Server, parentContext: parentSpan.Context);
+        
+        FieldInfo fieldInfo = typeof(TelemetrySpan).GetField(
+            "Activity",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+        Activity childActivity = (Activity)fieldInfo.GetValue(childSpan);
+        attributePropagatingSpanProcessor.OnStart(childActivity);
+        
 
         try
         {
@@ -64,23 +77,30 @@ public class AttributePropagatingSpanProcessorTest
         }
     }
 
-    private void validateSpanAttributesInheritance(TelemetrySpan parentSpan, string? propagatedName, string? propagationValue1, string? propagatedValue2)
+    private void validateSpanAttributesInheritance(
+        TelemetrySpan parentSpan,
+        string? propagatedName,
+        string? propagationValue1,
+        string? propagatedValue2)
     {
         TelemetrySpan leafSpan = createNestedSpan(parentSpan, 10);
-        
+
         Assert.True(leafSpan.ParentSpanId != default);
-        FieldInfo fieldInfo = typeof(TelemetrySpan).GetField("Activity", BindingFlags.NonPublic | BindingFlags.Instance);
+        FieldInfo fieldInfo = typeof(TelemetrySpan).GetField(
+            "Activity",
+            BindingFlags.NonPublic | BindingFlags.Instance);
         Activity leafActivity = (Activity)fieldInfo.GetValue(leafSpan);
-        
+
         Assert.Equal(leafActivity.DisplayName, "child:1");
         if (propagatedName != null)
         {
-            Assert.Equal(leafActivity.GetTagItem(spanNameKey), propagatedName);
+            Assert.Equal(propagatedName, leafActivity.GetTagItem(spanNameKey));
         }
         else
         {
             Assert.Null(leafActivity.GetTagItem(spanNameKey));
         }
+
         if (propagationValue1 != null)
         {
             Assert.Equal(leafActivity.GetTagItem(testKey1), propagationValue1);
@@ -89,6 +109,7 @@ public class AttributePropagatingSpanProcessorTest
         {
             Assert.Null(leafActivity.GetTagItem(testKey1));
         }
+
         if (propagatedValue2 != null)
         {
             Assert.Equal(leafActivity.GetTagItem(testKey2), propagatedValue2);
