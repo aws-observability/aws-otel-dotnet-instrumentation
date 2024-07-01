8bc077d526def63a9199cc27ea61aafcbf3dd502
Yiran Wang <yiranwan@amazon.com>
2024-07-01 10:54:40 -0700

    Move test application images to new folder

--- test/images/Applications/TestSimpleApp.EfCore/Data/Blog.cs
+++
@@ -1,9 +0,0 @@
-namespace TestSimpleApp.EfCore.Data;
-
-public class Blog
-{
-    public int BlogId { get; set; }
-    public required string Url { get; set; }
-
-    public IList<Post> Posts { get; } = [];
-}
