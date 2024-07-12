6f7bba1ad1199d800376bfe5a610641e93c83926
Xinran Zhang <xinranzh@amazon.com>
2024-07-12 10:31:18 -0700

    to store version in file

---
+++ src/AWS.Distro.OpenTelemetry.AutoInstrumentation/Version.cs
@@ -0,0 +1,11 @@
+// <copyright file="Version.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AWS.Distro.OpenTelemetry.AutoInstrumentation;
+
+
+static class Version
+{
+    public static string version = "0.0.0";
+}
