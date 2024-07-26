95d6d6bf23f882a6fb3a771b6126006618129ca7
Vastin <3690049+vastin@users.noreply.github.com>
2024-07-26 17:32:16 +0000

    Add copyright header in version file

--- src/AWS.Distro.OpenTelemetry.AutoInstrumentation/Version.cs
+++ src/AWS.Distro.OpenTelemetry.AutoInstrumentation/Version.cs
@@ -1,9 +1,7 @@
-// <copyright file="Version.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 namespace AWS.Distro.OpenTelemetry.AutoInstrumentation;
-
 
 static class Version
 {
