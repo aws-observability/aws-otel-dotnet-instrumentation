b026298c19367b1d583635fd576e5b4b8606aafc
Eric Zhang <eric.zhang6811@gmail.com>
2024-10-30 19:21:42 +0000

    support Anthropic Claude and Meta Llama models

--- src/OpenTelemetry.Instrumentation.AWS/Implementation/AWSLlmModelProcessor.cs
+++ src/OpenTelemetry.Instrumentation.AWS/Implementation/AWSLlmModelProcessor.cs
@@ -34,6 +34,12 @@ internal class AWSLlmModelProcessor
                         case "amazon.titan":
                             ProcessTitanModelRequestAttributes(activity, jsonObject);
                             break;
+                        case "anthropic.claude":
+                            ProcessClaudeModelRequestAttributes(activity, jsonObject);
+                            break;
+                        case "meta.llama3":
+                            ProcessLlamaModelRequestAttributes(activity, jsonObject);
+                            break;
                     }
                 }
                 catch (Exception ex)
@@ -70,6 +76,12 @@ internal class AWSLlmModelProcessor
                     {
                         case "amazon.titan":
                             ProcessTitanModelResponseAttributes(activity, jsonObject);
+                            break;
+                        case "anthropic.claude":
+                            ProcessClaudeModelResponseAttributes(activity, jsonObject);
+                            break;
+                        case "meta.llama3":
+                            ProcessLlamaModelResponseAttributes(activity, jsonObject);
                             break;
                     }
                 }
@@ -130,6 +142,98 @@ internal class AWSLlmModelProcessor
                 {
                     activity.SetTag(AWSSemanticConventions.AttributeGenAiFinishReasons, finishReasons.GetString());
                 }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+
+    private static void ProcessClaudeModelRequestAttributes(Activity activity, Dictionary<string, JsonElement> jsonBody)
+    {
+        try
+        {
+            if (jsonBody.TryGetValue("top_p", out var topP))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiTopP, topP.GetDouble());
+            }
+
+            if (jsonBody.TryGetValue("temperature", out var temperature))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiTemperature, temperature.GetDouble());
+            }
+
+            if (jsonBody.TryGetValue("max_tokens_to_sample", out var maxTokens))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiMaxTokens, maxTokens.GetInt32());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+
+    private static void ProcessClaudeModelResponseAttributes(Activity activity, Dictionary<string, JsonElement> jsonBody)
+    {
+        try
+        {
+            if (jsonBody.TryGetValue("stop_reason", out var finishReasons))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiFinishReasons, finishReasons.GetString());
+            }
+
+            // prompt_tokens and completion_tokens not provided in Claude response body.
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+
+    private static void ProcessLlamaModelRequestAttributes(Activity activity, Dictionary<string, JsonElement> jsonBody)
+    {
+        try
+        {
+            if (jsonBody.TryGetValue("top_p", out var topP))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiTopP, topP.GetDouble());
+            }
+
+            if (jsonBody.TryGetValue("temperature", out var temperature))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiTemperature, temperature.GetDouble());
+            }
+
+            if (jsonBody.TryGetValue("max_gen_len", out var maxTokens))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiMaxTokens, maxTokens.GetInt32());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+
+    private static void ProcessLlamaModelResponseAttributes(Activity activity, Dictionary<string, JsonElement> jsonBody)
+    {
+        try
+        {
+            if (jsonBody.TryGetValue("prompt_token_count", out var promptTokens))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiPromptTokens, promptTokens.GetInt32());
+            }
+
+            if (jsonBody.TryGetValue("generation_token_count", out var completionTokens))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiCompletionTokens, completionTokens.GetInt32());
+            }
+
+            if (jsonBody.TryGetValue("stop_reason", out var finishReasons))
+            {
+                activity.SetTag(AWSSemanticConventions.AttributeGenAiFinishReasons, finishReasons.GetString());
             }
         }
         catch (Exception ex)
